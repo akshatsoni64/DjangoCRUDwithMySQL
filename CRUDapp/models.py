139e17e4b690f949166dde2ddb23a38fb8c643f0
akshatsoni64 <akshatsoni64@gmail.com>
2019-11-07 12:26:44 +0530

    Migrated to MySQL and Model Done

--- CRUDapp/models.py
+++ CRUDapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    branch = models.CharField(max_length=20)
+    eno = models.TextField(max_length=10)
